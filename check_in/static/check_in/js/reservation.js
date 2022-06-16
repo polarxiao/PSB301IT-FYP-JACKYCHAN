8f25660c159cc4cd1aa623d465615f2a3ba93f88
polarxiao <56618586+polarxiao@users.noreply.github.com>
2022-06-16 23:55:35 +0800

    Delete check_in directory

--- check_in/static/check_in/js/reservation.js
+++
@@ -1,5 +0,0 @@
-// event when reservation thumbnail is clicked
-$('.thumbnail-container').click(function() {
-    $(this).find('input').prop('checked', true).change();
-    $('#form-reservation').submit();
-});
